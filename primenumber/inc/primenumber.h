02492d834d5e333613393f6a05dd9b068544647c
NIRANJAN KUMAR M <37568654+stepin104545@users.noreply.github.com>
2020-08-13 16:34:44 +0530

    Create primenumber.h

---
+++ primenumber/inc/primenumber.h
@@ -0,0 +1,17 @@
+/**
+* @file primenumber.h
+*
+*/
+#ifndef __PRIMENUMBER_H__
+#define __PRIMENUMBER_H__
+
+/**
+* Calculates the given number is prime or not
+* @param[in] number for which it checks for the number is prime or not
+* @return true if the given number is prime number
+* @return false if the given number is not a prime number
+* @note Returns false for the number is less the two
+*/
+int primenumber(int number);
+
+#endif 
